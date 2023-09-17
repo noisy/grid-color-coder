b231f0aa85a3c0b1f980543445cc063baddb531b
Krzysztof Szumny <noisy.pl@gmail.com>
2023-09-17 14:33:14 +0200

    initialization view

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 new Vue({
     el: '#app',
     data: {
-        colors: ['green', 'black', 'orange', 'red', 'white'],
-        selectedColor: 'green',
+        initialized: false,
+        gridSizeX: 11,
+        gridSizeY: 11,
+        newColor: '#000000',
+        colors: ['#FFFFFF'],
+        selectedColor: '#FFFFFF',
         coordinateInput: '',
         grid: Array(11).fill().map(() => Array(11).fill(''))
     },
     methods: {
+        addColor() {
+            if (this.colors.indexOf(this.newColor) === -1) {
+                this.colors.push(this.newColor);
+            }
+        },
+        initializeGrid() {
+            this.initialized = true;
+        },
         selectColor(color) {
             this.selectedColor = color;
         },
@@ -26,5 +38,19 @@ new Vue({
                 this.coordinateInput = '';
             }
         }
+    },
+    computed: {
+        getLetters() {
+            let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return letters.substring(0, this.gridSizeX).split('');
+        }
+    },
+    watch: {
+        gridSizeY(newVal) {
+            document.documentElement.style.setProperty('--grid-size-y', newVal);
+        }
+    },
+    created() {
+        document.documentElement.style.setProperty('--grid-size-y', this.gridSizeY);
     }
 });
